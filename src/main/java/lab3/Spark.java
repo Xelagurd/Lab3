3eb430ff7e6bd6c23b3a6bac1c3348ac616e2278
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-19 00:43:03 +0300

    Scripted auto-commit on change (2019-10-19 00:43:03) by gitwatch.sh

--- src/main/java/lab3/Spark.java
+++ src/main/java/lab3/Spark.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
 
 import java.util.Map;
@@ -47,5 +48,10 @@ public class Spark {
                     return new Tuple2<>(Integer.parseInt(splitAp[0]), splitAp[1]);
                 }
         ).collectAsMap();
+
+        /*ё. создаем в основном методе main переменную broadcast */
+        final Broadcast<Map<Integer, String>> broadcastAp = sc.broadcast(toPair);
+
+        
     }
 }
