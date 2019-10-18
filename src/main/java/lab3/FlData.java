53e0707dd64b64863b4e561d253d0632844bd1cf
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-19 00:24:45 +0300

    Scripted auto-commit on change (2019-10-19 00:24:44) by gitwatch.sh

--- src/main/java/lab3/FlData.java
+++ src/main/java/lab3/FlData.java
@@ -7,6 +7,9 @@ public class FlData implements Serializable {
     /*г. Создаем Java объекты для хранения данных — простые объекты реализующие интерфейс Serializable */
     private int departApId;
     private int arrivalApId;
+    private boolean delayed;
+    private boolean canceled;
+    private double delay;
     
     public int getDepartApId() {
         return departApId;
