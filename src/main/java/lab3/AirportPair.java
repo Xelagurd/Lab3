6e3aecf5e144b530880c0073e21587ee5ce45b14
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-19 00:34:03 +0300

    Scripted auto-commit on change (2019-10-19 00:34:03) by gitwatch.sh

--- src/main/java/lab3/AirportPair.java
+++ src/main/java/lab3/AirportPair.java
@@ -3,5 +3,16 @@ package lab3;
 import java.io.Serializable;
 
 public class AirportPair implements Serializable {
+    /*г. Создаем Java объекты для хранения данных — простые объекты реализующие интерфейс Serializable */
+    private int allFl;
+    private int canceledFl;
+    private int delayedFl;
+    private double maxDelay;
 
+    public AirportPair(int allFl, int canceledFl, int delayedFl, double maxDelay) {
+        this.allFl = allFl;
+        this.canceledFl = canceledFl;
+        this.delayedFl = delayedFl;
+        this.maxDelay = maxDelay;
+    }
 }
