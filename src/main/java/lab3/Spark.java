781f6b70794f76b99b824107d6172008f792c051
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-19 00:21:27 +0300

    Scripted auto-commit on change (2019-10-19 00:21:27) by gitwatch.sh

--- src/main/java/lab3/Spark.java
+++ src/main/java/lab3/Spark.java
@@ -1,10 +1,14 @@
 package lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class Spark {
     public static void main(String[] args) {
+        /*а. Инициализируем Spark */
         SparkConf conf = new SparkConf().setAppName("Lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
+        /*б. Загружаем исходные наборы данных в RDD*/
+        JavaRDD<String> flFile = sc.textFile("664600583_T_ONTIME_sample.csv");
     }
 }
