e7822f4572c423b34f41c9d9fcffa8cb0d1fc6a4
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-19 00:31:25 +0300

    Scripted auto-commit on change (2019-10-19 00:31:25) by gitwatch.sh

--- src/main/java/lab3/Spark.java
+++ src/main/java/lab3/Spark.java
@@ -1,8 +1,10 @@
 package lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class Spark {
     public static void main(String[] args) {
@@ -11,5 +13,9 @@ public class Spark {
         JavaSparkContext sc = new JavaSparkContext(conf);
         /*б. Загружаем исходные наборы данных в RDD*/
         JavaRDD<String> flFile = sc.textFile("664600583_T_ONTIME_sample.csv");
+        /*в. Преобразуем RDD в RDD пару ключ значение */
+        JavaPairRDD<Tuple2, FlData> flData = flFile.mapToPair(s -> {
+
+        });
     }
 }
